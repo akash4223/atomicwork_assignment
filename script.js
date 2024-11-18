8b51b7f6a41f79600e5ec645a5b912bdbb12432f
akash4223 <choudharyakash131@gmail.com>
2024-11-18 17:32:10 +0530

    done

--- script.js
+++ script.js
@@ -348,3 +348,104 @@ if (window.matchMedia("(min-width: 991px)").matches) {
 }
 
 /////////////////////ROI CALCULATOR////////////////////////
+let totalResources = document.querySelector("#resources");
+let totalComp = document.querySelector("#comp");
+let totalHours = document.querySelector("#time_needed");
+let queries = document.querySelector("#queries");
+let numberFields = [totalResources, totalHours, totalComp, queries];
+
+let moneySavedTxt = document.querySelector("#money_saved");
+let timeSavedTxt = document.querySelector("#time_saved");
+let calculatorErrTxt = document.querySelector(".calc-err-txt");
+
+let AtomicWorkCostPerEmp = 90; //annual cost in USD
+let timeTakenByTool = 5; //mins taken by Atomic work took to resolve a single query
+
+let calcSubBtn = document.querySelector(".calc-submit-button");
+let revealButton = document.querySelector("#reveal_btn");
+
+//accept only numbers in calculator fields
+numberFields.forEach(function (field) {
+  field.addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9]/g, "");
+  });
+});
+
+calcSubBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  //check if any field is left empty
+  let isEmpty = false;
+  let calcFormFields = Array.from(
+    document.querySelectorAll(".calc-form-field")
+  );
+
+  calcFormFields.forEach(function (field) {
+    if (field.tagName == "INPUT" && field.value.trim() == "") {
+      isEmpty = true;
+    } else {
+      isEmpty = false;
+    }
+  });
+
+  if (isEmpty) {
+    //throw error
+    calculatorErrTxt.style.opacity = 1;
+
+    setTimeout(() => {
+      calculatorErrTxt.style.opacity = 0;
+    }, 1000);
+  } else {
+    //Calculating money saved
+    //money saved == (total resources * comp of each resource) - cost of Atomicworks tool
+    let moneySaved =
+      totalResources.value * totalComp.value -
+      AtomicWorkCostPerEmp * totalResources.value;
+
+    moneySavedTxt.style.filter = "blur(12px)";
+
+    moneySavedTxt.innerHTML = Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+    }).format(moneySaved);
+
+    //Calculating time saved
+    //time saved =( (no. of queries generated annually/number of agents) * time taken by resource to resolve a query )
+    //This amount of time was dedicated by humans for solving queries, now this will be taken care of by machine
+    let timeSaved = (queries.value / totalResources.value) * totalHours.value;
+    timeSavedTxt.innerHTML = `${timeSaved.toFixed(2)} hrs`;
+
+    //show revel data button
+    revealButton.style.display = "block";
+  }
+});
+
+//////////////////VALIDATE GET REPORT FIELD/////////////////
+let reportSubmitBtn = document.querySelector(".get-report-submit-button");
+let reportEmailField = document.querySelector(".get-report-form-field");
+let reportErrorText = document.querySelector(".get-report-err_txt");
+
+reportSubmitBtn.addEventListener("click", (e) => {
+  //check if email is valid or not
+  if (emailRegex.test(reportEmailField.value.trim())) {
+  } else {
+    e.preventDefault();
+    reportErrorText.style.display = "block";
+    reportErrorText.innerHTML = `Please enter a valid work email ID`;
+  }
+
+  //check if it is a work email, prevent submission if personal email
+  let emailDomain = reportEmailField.value.trim().split("@")[1];
+  if (personalEmailDomains.includes(emailDomain)) {
+    e.preventDefault();
+    reportErrorText.style.display = "block";
+    reportErrorText.innerHTML = `We only accept work email IDs`;
+  } else {
+  }
+});
+
+//remove error text when field is focused
+reportEmailField.addEventListener("focusin", (e) => {
+  reportErrorText.style.display = "none";
+});
